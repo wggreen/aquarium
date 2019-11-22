287e6683c896d1ac2cf2a4867c87b1168bf5625e
wggreen <wggreen.nss@gmail.com>
2019-11-22 10:52:19 -0600

    imported fish data provider

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -10,52 +10,60 @@ const fishCollection = [
         species: "Orange Clownfish",
         location: "Caribbean",
         length: 3,
-        food: ["algae", "crustaceans"]
+        food: ["algae", "crustaceans"],
         image: "styles\main\fish-1.jpg"
-    }
+    },
     {
         name: "Bluey",
         species: "Blue Clownfish",
         location: "Fiji",
         length: 4,
-        food: ["shrimp", "coral"]
+        food: ["shrimp", "coral"],
         image: "styles\main\fish-2.jpg"
 
-    }
+    },
     {
         name: "Old Yeller",
         species: "Yellow Clownfish",
         location: "Indian Ocean",
         length: 2,
-        food: ["kelp", "plankton"]
+        food: ["kelp", "plankton"],
         image: "styles\main\fish-3.jpg"
 
-    }
+    },
     {
         name: "Bighead",
         species: "Orange Goldfish",
         location: "Gulf of Mexico",
         length: 1,
-        food: ["flakes"]
+        food: ["flakes"],
         image: "styles\main\fish-4.jpg"
 
-    }
+    },
     {
         name: "Scaly",
         species: "Scaly Clownfish",
         location: "Bora Bora",
         length: 5,
-        food: ["whales", "sharks"]
+        food: ["whales", "sharks"],
         image: "styles\main\fish-5.jpg"
 
-    }
+    },
     {
         name: "Twins",
         species: "Conjoined Clownfish",
         location: "Chernobyl",
         length: 7,
-        food: ["toxic waste"]
+        food: ["toxic waste"],
         image: "styles\main\fish-6.jpg"
 
     }
 ]   
+
+// This is new code. Add this.
+export const useFish = () => {
+    return fishCollection
+}
+
+//immutability: our core. raw data should be private and hidden and should not be changed by other modules
+//this is why we are exporting this FUNCTION (useFish) instead of just the array
--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,16 @@
 import initializeDetailButtonEvents from './dialogs.js'
 import addMouseOverEventListeners from './highlight.js'
 import addMouseOverBorder from './border.js'
+import { useFish } from './FishDataProvider.js'
 
 initializeDetailButtonEvents()
 addMouseOverEventListeners()
 addMouseOverBorder()
+
+const allOfTheFish = useFish()
+
+console.log("all of the fish:", allOfTheFish)
+
+for (const fish of allOfTheFish) {
+    console.log("individual fish object:", fish)
+}
